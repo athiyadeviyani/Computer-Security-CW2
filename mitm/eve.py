837626033c0da8846a61d5079fe758429590c530
Athiya Deviyani <athiyadee@gmail.com>
2020-03-03 20:35:15 +0000

    Added sys.exit(1) if wrong flags

--- mitm/eve.py
+++ mitm/eve.py
@@ -13,6 +13,10 @@ parser.add_argument("--break-heart", help="Alice is going to break Bob's heart."
 parser.add_argument("--custom", help="Send custom message.", action="store_true")
 args = parser.parse_args()
 
+if len(sys.argv) != 2:
+    print("You must pick exactly one of the following flags: --relay, --break-heart, --custom")
+    sys.exit(1)
+
 if args.relay:
     print("EVE IS ON RELAY MODE")
 elif args.break_heart:
